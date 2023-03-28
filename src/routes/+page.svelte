85cb2613d9e3af2e4f8cb985c47f85d51fddd360
jaeschke <jaeschke@fim.uni-passau.de>
2023-03-28 09:38:39 +0200

    Added the extendendedWorld-class which keeps track of the scene and its cars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,8 @@
 	import * as THREE from 'three';
 	import CannonDebugger from 'cannon-es-debugger';
 	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-	import { RigidVehicle } from 'cannon-es';
+	import Stats from 'three/examples/jsm/libs/stats.module';
+	import { Material, RigidVehicle, Vec3, World } from 'cannon-es';
 	import { onMount } from 'svelte';
 
 	// Detects webgl
@@ -19,35 +20,58 @@ document.getElementById( 'container' ).innerHTML = "";
 }
 */
 
-	// - Global variables -
-	var ZERO_QUATERNION = new THREE.Quaternion(0, 0, 0, 1);
+	var stats: any;
 
 	// Graphics variables
 	var container: HTMLElement | null;
 	var camera: any, controls, scene: any, renderer: any;
-	var texture;
-	var clock = new THREE.Clock();
 	var materialDynamic, materialStatic, materialInteractive: any;
 	var XPointer: any;
 
 	// Physics variables
-	var world: any;
-	var cannonDebugRenderer: any;
-	var bodyMaterial;
-	var wheelMaterial: any;
-	var groundMaterial;
+	var worlds: any[] = [];
+	var groundBodyContactMaterialOptions = {
+		friction: 0.9,
+		restitution: 0.1,
+		contactEquationRelaxation: 3,
+		frictionEquationStiffness: 1e8
+	};
+	var groundWheelContactMaterialOptions = {
+		friction: 0.9,
+		restitution: 0.1,
+		contactEquationRelaxation: 3,
+		frictionEquationStiffness: 1e8
+	};
+	var worldOptions = {
+		allowSleep: true,
+		quatNormalizeFast: true,
+		quatNormalizeSkip: 1
+	};
+	var gravity = -9.82;
+
+	//Track matrix
+	// Create a matrix of height values
+	const matrix: number[][] = [];
+	const sizeX = 20;
+	const sizeZ = 20;
+	for (let i = 0; i < sizeX; i++) {
+		matrix.push([]);
+		for (let j = 0; j < sizeZ; j++) {
+			if (i === 0 || i === sizeX - 1 || j === 0 || j === sizeZ - 1) {
+				let height2: number = 3;
+				matrix[i].push(height2);
+				continue;
+			}
+
+			const height3 = Math.cos((i / sizeX) * Math.PI * 2) * Math.cos((j / sizeZ) * Math.PI * 2) + 2;
+			matrix[i].push(height3 * 4);
+		}
+	}
 
 	//Collision Groups
 	var GROUP1 = 1;
 	var GROUP2 = 2;
-
-	//carList
-	var carList: ExtendedRigidVehicle[] = [];
-
-	var time = 0;
-
-	// Keybord actions
-	var actions = {};
+	var GROUP3 = 4;
 
 	function initGraphics() {
 		container = document.getElementById('container');
@@ -80,6 +104,11 @@ document.getElementById( 'container' ).innerHTML = "";
 			container.innerHTML = '';
 			controls = new OrbitControls(camera, container);
 			container.appendChild(renderer.domElement);
+
+			stats = Stats();
+			stats.domElement.style.position = 'absolute';
+			stats.domElement.style.top = '0px';
+			container.appendChild(stats.domElement);
 		}
 
 		var geometryX = new THREE.PlaneGeometry(10, 1, 1);
@@ -118,163 +147,72 @@ document.getElementById( 'container' ).innerHTML = "";
 	}
 
 	/**
-	 * Physics
-	 **/
-
-	function initPhysics() {
-		var optiones = {
-			allowSleep: true,
-			quatNormalizeFast: true,
-			quatNormalizeSkip: 1
-		};
-
-		world = new CANNON.World(optiones);
-		world.broadphase = new CANNON.SAPBroadphase(world);
-		world.gravity.set(0, -9.82, 0);
-		world.defaultContactMaterial.friction = 1;
-
-		cannonDebugRenderer = CannonDebugger(scene, world);
-
-		groundMaterial = new CANNON.Material('groundMaterial');
-		wheelMaterial = new CANNON.Material('wheelMaterial');
-		bodyMaterial = new CANNON.Material('bodyMaterial');
-		var wheelGroundContactMaterial = new CANNON.ContactMaterial(wheelMaterial, groundMaterial, {
-			friction: 0.5,
-			restitution: 0.3,
-			contactEquationRelaxation: 3,
-			frictionEquationStiffness: 10000
-		});
-
-		var bodyGroundContactMaterial = new CANNON.ContactMaterial(bodyMaterial, groundMaterial, {
-			friction: 0.9,
-			restitution: 0.1,
-			contactEquationRelaxation: 3,
-			frictionEquationStiffness: 1e8
-		});
-
-		world.addContactMaterial(wheelGroundContactMaterial);
-		world.addContactMaterial(bodyGroundContactMaterial);
-
-		var q = XPointer.quaternion;
-		var planeBody = new CANNON.Body({
-			mass: 0, // mass = 0 makes the body static
-			material: groundMaterial,
-			shape: new CANNON.Plane(),
-			quaternion: new CANNON.Quaternion(-q._x, q._y, q._z, q._w),
-			collisionFilterGroup: GROUP2,
-			collisionFilterMask: GROUP1
-		});
-
-		world.addBody(planeBody);
-	}
-
-	function initTrack() {
-		// Create a matrix of height values
-		const matrix: number[][] = [];
-		const sizeX = 20;
-		const sizeZ = 20;
-		for (let i = 0; i < sizeX; i++) {
-			matrix.push([]);
-			for (let j = 0; j < sizeZ; j++) {
-				if (i === 0 || i === sizeX - 1 || j === 0 || j === sizeZ - 1) {
-					let height2: number = 3;
-					matrix[i].push(height2);
-					continue;
-				}
-
-				const height3 =
-					Math.cos((i / sizeX) * Math.PI * 2) * Math.cos((j / sizeZ) * Math.PI * 2) + 2;
-				matrix[i].push(height3 * 4);
-			}
-		}
-
-		// Create the heightfield
-		const heightfieldShape = new CANNON.Heightfield(matrix, {
-			elementSize: 5
-		});
-		const heightfieldBody = new CANNON.Body({
-			mass: 0,
-			collisionFilterGroup: 2,
-			collisionFilterMask: 1
-		});
-		heightfieldBody.addShape(heightfieldShape);
-		heightfieldBody.position.set(-5, -2, ((sizeZ - 1) * heightfieldShape.elementSize) / 2);
-		heightfieldBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
-		world.addBody(heightfieldBody);
-
-		//
-	}
-
-	/**
-	 * init a car
+	 * Pyhsics
 	 */
 
-	function createExtendedCar() {
-		const lengthX = 4;
-		const lengthY = 1;
-		const lengthZ = 2;
-
-		let vehicle = new ExtendedRigidVehicle(lengthX, lengthY, lengthZ);
-
-		const axisWidth = 4;
-		const radius = 2;
-		const width = 0.5;
-
-		for (var i = 0; i < 4; i++) {
-			vehicle.addWheelWithMesh(
-				radius,
-				width,
-				-4 + Math.random() * 9,
-				0,
-				-2 + Math.random() * 5,
-				scene
+	function initPhysics() {
+		for (var i = 0; i < population / carsPerWorld; i++) {
+			var world = new ExtendedWorld(
+				scene,
+				worldOptions,
+				gravity,
+				groundBodyContactMaterialOptions,
+				groundWheelContactMaterialOptions
 			);
-		}
+			//world.initTrack(matrix);
 
-		carList.push(vehicle);
-		vehicle.addToWorld(world);
+			//add Cars
+			for (var j = 0; j < carsPerWorld; j++) {
+				world.addCar(5, 1, 2);
+			}
+
+			worlds.push(world);
+
+			var chassisShape = new CANNON.Box(new CANNON.Vec3(10, 0.1, 50));
+			var box = new CANNON.Body({
+				mass: 0,
+				position: new CANNON.Vec3(40, 2, 0),
+				collisionFilterGroup: GROUP3,
+				collisionFilterMask: GROUP1
+			});
+			const rotateParallelToZAxis = new CANNON.Quaternion().setFromEuler(0, 0, 0.4);
+			box.addShape(chassisShape, new CANNON.Vec3(), rotateParallelToZAxis);
+			world.addBody(box);
+		}
+		console.log(worlds);
 	}
 
 	/**
 	 * Main
 	 **/
 
+	const frameTime: number = 1 / 60;
+	const delta: number = 1; //???
+
 	function updatePhysics() {
-		world.step(1 / 60);
-
 		// update the chassis position
-
-		carList.forEach((car) => {
-			const posBody = car.chassisBody.position;
-			const quatBody = car.chassisBody.quaternion;
-			car.carVisualBody.position.set(posBody.x, posBody.y, posBody.z);
-			car.carVisualBody.quaternion.set(quatBody.x, quatBody.y, quatBody.z, quatBody.w);
-			for (var i = 0; i < car.wheelBodies.length; i++) {
-				const posWheel = car.wheelBodies[i].position;
-				const quatWheel = car.wheelBodies[i].quaternion;
-				car.wheelMeshes[i].position.set(posWheel.x, posWheel.y, posWheel.z);
-				car.wheelMeshes[i].quaternion.set(quatWheel.x, quatWheel.y, quatWheel.z, quatWheel.w);
-			}
+		worlds.forEach((world) => {
+			world.updatePhysicsWithScene(frameTime);
 		});
 	}
 
 	function render() {
 		requestAnimationFrame(render);
 		updatePhysics();
-		cannonDebugRenderer.update();
+		worlds.forEach((world) => {
+			world.cannonDebugRenderer.update();
+		});
 		renderer.render(scene, camera);
+		stats.update();
 	}
 
-	var population: number = 20;
+	var population: number = 30;
+	var carsPerWorld: number = 30;
 
 	onMount(() => {
 		initGraphics();
 		initPhysics();
-		initTrack();
 
-		for (var i = 0; i < population; i++) {
-			createExtendedCar();
-		}
 		render();
 	});
 
@@ -282,19 +220,25 @@ document.getElementById( 'container' ).innerHTML = "";
 		wheelMeshes: Mesh[] = [];
 		carVisualBody: Mesh;
 
-		constructor(lengthX: number, lengthY: number, lengthZ: number) {
+		constructor(lengthX: number, lengthY: number, lengthZ: number, bodyMaterial: CANNON.Material) {
 			super();
-			this.carVisualBody = this.addCarBodyMesh(lengthX, lengthY, lengthZ);
+			this.carVisualBody = this.addCarBodyMesh(lengthX, lengthY, lengthZ, bodyMaterial);
 		}
 
-		addCarBodyMesh(lengthX: number, lengthY: number, lengthZ: number) {
+		addCarBodyMesh(
+			lengthX: number,
+			lengthY: number,
+			lengthZ: number,
+			bodyMaterial: CANNON.Material
+		) {
 			//Add physical Body
 			var chassisShape = new CANNON.Box(new CANNON.Vec3(lengthX, lengthY, lengthZ));
 			var chassisBody = new CANNON.Body({
 				mass: 10,
 				position: new CANNON.Vec3(0, 5, 0),
+				material: bodyMaterial,
 				collisionFilterGroup: GROUP1,
-				collisionFilterMask: GROUP2
+				collisionFilterMask: GROUP2 | GROUP3
 			});
 			chassisBody.addShape(chassisShape);
 			this.chassisBody = chassisBody;
@@ -306,7 +250,9 @@ document.getElementById( 'container' ).innerHTML = "";
 				side: THREE.DoubleSide
 			});
 			var box = new THREE.Mesh(geometry, material);
-			scene.add(box);
+			if (scene !== null) {
+				scene.add(box);
+			}
 			return box;
 		}
 
@@ -316,14 +262,15 @@ document.getElementById( 'container' ).innerHTML = "";
 			positionX: number,
 			positionY: number,
 			positionZ: number,
-			scene: any
+			scene: any,
+			wheelMaterial: CANNON.Material
 		) {
 			var wheelVolume = Math.PI * width * (radius * radius);
 			let wheelBody = new CANNON.Body({
 				mass: wheelVolume,
 				material: wheelMaterial,
 				collisionFilterGroup: GROUP1,
-				collisionFilterMask: GROUP2
+				collisionFilterMask: GROUP2 | GROUP3
 			});
 
 			const rotateParallelToXAxis = new CANNON.Quaternion().setFromEuler(Math.PI / 2, 0, 0);
@@ -355,8 +302,133 @@ document.getElementById( 'container' ).innerHTML = "";
 				)
 			);
 
-			scene.add(mesh);
+			if (scene !== null) {
+				scene.add(mesh);
+			}
 			this.wheelMeshes.push(mesh);
+		}
+	}
+
+	class ExtendedWorld extends World {
+		scene: any;
+		cars: ExtendedRigidVehicle[] = [];
+		cannonDebugRenderer: any;
+		groundMaterial: CANNON.Material = new CANNON.Material('groundMaterial');
+		wheelMaterial: CANNON.Material = new CANNON.Material('wheelMaterial');
+		bodyMaterial: CANNON.Material = new CANNON.Material('bodyMaterial');
+
+		constructor(
+			scene: any,
+			options: any,
+			gravity: number,
+			groundBodyContactMaterialOptions: any,
+			groundWheelContactMaterialOptions: any
+		) {
+			super(options);
+			this.scene = scene;
+			this.gravity.set(0, gravity, 0);
+			this.initPhysics(groundBodyContactMaterialOptions, groundWheelContactMaterialOptions);
+
+			//Uncomment for debug information
+			this.cannonDebugRenderer = CannonDebugger(this.scene, this);
+		}
+
+		addCar(lengthX: number, lengthY: number, lengthZ: number) {
+			let vehicle = new ExtendedRigidVehicle(lengthX, lengthY, lengthZ, this.bodyMaterial);
+
+			const axisWidth = 4;
+			const radius = 2;
+			const width = 0.5;
+
+			for (var i = 0; i < 4; i++) {
+				vehicle.addWheelWithMesh(
+					radius,
+					width,
+					-4 + Math.random() * 9,
+					0,
+					-2 + Math.random() * 5,
+					this.scene,
+					this.wheelMaterial
+				);
+			}
+
+			this.cars.push(vehicle);
+			vehicle.addToWorld(this);
+		}
+
+		updatePhysicsWithScene(frameTime: number) {
+			//world.step(frameTime, delta, 1);
+			this.step(frameTime);
+
+			//update position of cars inside the scene
+			this.cars.forEach((car) => {
+				const posBody = car.chassisBody.position;
+				const quatBody = car.chassisBody.quaternion;
+				car.carVisualBody.position.set(posBody.x, posBody.y, posBody.z);
+				car.carVisualBody.quaternion.set(quatBody.x, quatBody.y, quatBody.z, quatBody.w);
+				for (var i = 0; i < car.wheelBodies.length; i++) {
+					const posWheel = car.wheelBodies[i].position;
+					const quatWheel = car.wheelBodies[i].quaternion;
+					car.wheelMeshes[i].position.set(posWheel.x, posWheel.y, posWheel.z);
+					car.wheelMeshes[i].quaternion.set(quatWheel.x, quatWheel.y, quatWheel.z, quatWheel.w);
+				}
+			});
+		}
+
+		updatePhysicsWithoutScene(frameTime: number) {
+			//world.step(frameTime, delta, 1);
+			this.step(frameTime);
+		}
+
+		initPhysics(bodyGroundOptiones: any, wheelGroundOptions: any) {
+			this.broadphase = new CANNON.SAPBroadphase(this);
+			this.defaultContactMaterial.friction = 1;
+			var wheelGroundContactMaterial = new CANNON.ContactMaterial(
+				this.wheelMaterial,
+				this.groundMaterial,
+				wheelGroundOptions
+			);
+
+			var bodyGroundContactMaterial = new CANNON.ContactMaterial(
+				this.bodyMaterial,
+				this.groundMaterial,
+				bodyGroundOptiones
+			);
+
+			this.addContactMaterial(wheelGroundContactMaterial);
+			this.addContactMaterial(bodyGroundContactMaterial);
+
+			var q = XPointer.quaternion;
+			var planeBody = new CANNON.Body({
+				mass: 0, // mass = 0 makes the body static
+				material: this.groundMaterial,
+				shape: new CANNON.Plane(),
+				quaternion: new CANNON.Quaternion(-q._x, q._y, q._z, q._w),
+				collisionFilterGroup: GROUP2,
+				collisionFilterMask: GROUP1
+			});
+
+			this.addBody(planeBody);
+		}
+
+		initTrack(matrix: number[][]) {
+			// Create the heightfield
+			const heightfieldShape = new CANNON.Heightfield(matrix, {
+				elementSize: 10
+			});
+			const heightfieldBody = new CANNON.Body({
+				mass: 0,
+				collisionFilterGroup: 2,
+				collisionFilterMask: 1
+			});
+			heightfieldBody.addShape(heightfieldShape);
+			heightfieldBody.position.set(
+				-5,
+				-2,
+				((matrix[0].length - 1) * heightfieldShape.elementSize) / 2
+			);
+			heightfieldBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
+			this.addBody(heightfieldBody);
 		}
 	}
 </script>
